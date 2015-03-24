07b7f8f0717ce84891bb5e1a8c5c1f30ece94b80
BVermet <bryan.vermet@student.howest.be>
2015-03-24 13:29:02 +0100

    Initial commit

---
+++ app/src/main/java/be/howest/nmct/colorpicker/ColorViewFragment.java
@@ -0,0 +1,138 @@
+package be.howest.nmct.colorpicker;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ColorViewFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ColorViewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ColorViewFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    static final String SS_COLOR="color";
+    private ColorView myColorView;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ColorViewFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ColorViewFragment newInstance(String param1, String param2) {
+        ColorViewFragment fragment = new ColorViewFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ColorViewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_color_view,container,false);
+        myColorView = (ColorView)v.findViewById(R.id.myColorView1);
+
+        if (savedInstanceState != null) {
+            Log.d("ColorView", "OnRestore: " + savedInstanceState.getString(SS_COLOR));
+            myColorView.setColor(savedInstanceState.getString(SS_COLOR));
+        }
+        return v;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(SS_COLOR, myColorView.getColor());
+    }
+
+    //Alternatieve code bij saven in de custom view zelf
+/*
+    @Override
+    protected Parcelable onSaveInstanceState () {
+        Parcelable superSaveState = super.onSaveInstanceState ();
+        Bundle saveState = new Bundle();
+        saveState. putParcelable (SS_SUPER_SAVE_STATE, superSaveState);
+        saveState. putString (SS_COLOR, getColor ());
+        return saveState;
+    }
+*/
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+/*
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+*/
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
